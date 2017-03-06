c7b4f3f44d5000a3ba6ceb9c92e9276941455aa2
Florian Fuchs <florian.fuchs@viovendi.de>
2017-03-06 17:51:36 +0100

    adjusted for doo use cases

--- sqs-dead-letter-requeue/main.go
+++ sqs-dead-letter-requeue/main.go
@@ -12,14 +12,18 @@ import (
 var (
 	app       = kingpin.New("dead-letter-requeue", "Requeues messages from a SQS dead-letter queue to the active one.")
 	queueName = app.Arg("queue-name", "Name of the SQS queue (e.g. prod-mgmt-website-data-www100-jimdo-com).").Required().String()
+        deadLetterQueueName = app.Arg("dead-letter-queue-name", "Name of the SQS queue (e.g. prod-mgmt-website-data-www100-jimdo-com-dead-letter).").Required().String()
+        cycles = app.Arg("cycles", "Number of cycles to requeue").Required().Int()
 )
 
 func main() {
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 
-	activeQueueName := *queueName
+	cycles := *cycles
+         
+        activeQueueName := *queueName
 
-	var deadLetterQueueName = activeQueueName + "_dead_letter"
+	deadLetterQueueName := *deadLetterQueueName
 
 	auth, err := aws.EnvAuth()
 	if err != nil {
@@ -27,7 +31,7 @@ func main() {
 		return
 	}
 
-	conn := sqs.New(auth, aws.EUWest)
+	conn := sqs.New(auth, aws.EUCentral)
 
 	deadLetterQueue, err := conn.GetQueue(deadLetterQueueName)
 	if err != nil {
@@ -42,8 +46,10 @@ func main() {
 	}
 
 	log.Printf("Looking for messages to requeue.")
-	for {
-		resp, err := deadLetterQueue.ReceiveMessageWithParameters(
+	iterations := 0
+        for iterations < cycles {
+		iterations += 1
+                resp, err := deadLetterQueue.ReceiveMessageWithParameters(
 			map[string]string{
 				"WaitTimeSeconds":     "20",
 				"MaxNumberOfMessages": "10",
